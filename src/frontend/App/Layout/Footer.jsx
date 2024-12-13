b4d34a91116ce7ce80b7cae5cda4df8bb11b7b87
Mitch Gohman <meatched+ghub-mac4@gmail.com>
2024-12-13 14:05:32 -0800

    Footer

--- src/frontend/App/Layout/Footer.jsx
+++ src/frontend/App/Layout/Footer.jsx
@@ -1,9 +1,52 @@
 import styled from "styled-components";
 
+// styles
+// FIXME: Does Styles Components offer a way to access this object through the Provider?
+import { theme } from "@Theme/theme";
+
+// components
+import Logo from "@CoreComponents/Logo";
+
+// styles
+const FooterStyled = styled.footer`
+    padding: 20px 0px;
+
+    text-align: center;
+
+    .logo {
+        display: flex;
+        justify-content: center;
+        img {
+            margin-bottom: 10px;
+            width: 150px;
+        }
+    }
+
+    h2 {
+        color: ${theme.colors.secondary.light};
+        margin: 5px 0px;
+        font-size: 14px;
+    }
+
+    .copyright {
+        font-size: 14px;
+        color: ${theme.colors.secondary.light};
+    }
+`;
+
 const Footer = () => {
-    return <FooterStyled>Footer</FooterStyled>;
+    return (
+        <FooterStyled>
+            <div className="logo">
+                <Logo />
+            </div>
+            <h2>
+                CrossFit Decimate :: Peace, Love, Unity, Fitness, Chill, and
+                Respect :: Colorado Springs, CO
+            </h2>
+            <div className="copyright">&copy; 2024. All rights reserved.</div>
+        </FooterStyled>
+    );
 };
 
 export default Footer;
-
-const FooterStyled = styled.footer``;
