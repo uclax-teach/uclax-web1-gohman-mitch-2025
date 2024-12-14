70bb3bea68a9104dffc93272341d3eac20fcd1dd
Mitch Gohman <mitch@thenounproject.com>
2024-12-14 10:05:47 -0700

    Responsive: Adding lesson content

--- src/frontend/App/Pages/CourseWork/Subpages/ResponsiveDesign.jsx
+++ src/frontend/App/Pages/CourseWork/Subpages/ResponsiveDesign.jsx
@@ -3,10 +3,40 @@ import styled from "styled-components";
 import { useMediaQuery } from "@Theme/media/MediaQueryContext";
 
 const ResponsiveDesignStyled = styled.div`
-    background-color: orange;
+    @media ${({ theme }) => theme.cssMedia.isMediumAndUp} {
+        .boxes {
+            display: flex;
+            justify-content: center;
+        }
+    }
+
+    .box {
+        width: 100%;
+        max-width: 200px;
+        height: 200px;
+        line-height: 200px;
+        font-size: 50px;
+        text-align: center;
+        color: white;
+        background-color: teal;
+        margin: 10px;
+    }
+    .box2 {
+        border-radius: 40px;
+    }
+    .box3 {
+        border-radius: 100px;
+    }
 
     @media ${({ theme }) => theme.cssMedia.isMediumAndUp} {
-        background-color: pink;
+        .box {
+            background-color: indigo;
+        }
+    }
+    @media ${({ theme }) => theme.cssMedia.isLarge} {
+        .box {
+            background-color: green;
+        }
     }
 `;
 
@@ -16,6 +46,12 @@ const ResponsiveDesign = () => {
     return (
         <ResponsiveDesignStyled>
             <h2>Responsive Design</h2>
+
+            <div className="boxes">
+                {jsMedia.isMediumAndUp && <div className="box box1">1</div>}
+                <div className="box box2">2</div>
+                <div className="box box3">3</div>
+            </div>
         </ResponsiveDesignStyled>
     );
 };
