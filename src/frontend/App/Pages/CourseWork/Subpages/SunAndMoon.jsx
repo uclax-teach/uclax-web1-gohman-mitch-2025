c40bc69248c0cd8f946e830af0bfddf788126467
Mitch Gohman <mitch@thenounproject.com>
2024-12-14 10:09:33 -0700

    Responsive: Adding Sun and Moon content

--- src/frontend/App/Pages/CourseWork/Subpages/SunAndMoon.jsx
+++ src/frontend/App/Pages/CourseWork/Subpages/SunAndMoon.jsx
@@ -1,8 +1,50 @@
+import styled from "styled-components";
+import { useState } from "react";
+
+// styles
+const SunAndMoonStyled = styled.div`
+    text-align: center;
+
+    h2 {
+        font-size: 30px;
+        color: teal;
+    }
+
+    img {
+        display: block;
+        margin: 30px auto;
+        padding: 20px;
+        border: solid 20px teal;
+        background-color: #004e4e;
+    }
+`;
+
+// data
+const sunSrc = "/assets/sunAndMoon/sun.png";
+const moonSrc = "/assets/sunAndMoon/moon.png";
+
+// component
 const SunAndMoon = () => {
+    // JS Layer
+    const [imgSrc, imgSrcUpdate] = useState(moonSrc);
+
+    const changeToSun = () => {
+        imgSrcUpdate(sunSrc);
+    };
+    const changeToMoon = () => {
+        imgSrcUpdate(moonSrc);
+    };
+
+    // HTML Layer
     return (
-        <div>
+        <SunAndMoonStyled>
             <h2>SunAndMoon</h2>
-        </div>
+            <img
+                src={imgSrc}
+                onMouseEnter={changeToSun}
+                onMouseLeave={changeToMoon}
+            />
+        </SunAndMoonStyled>
     );
 };
 
